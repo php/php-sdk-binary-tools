c56dfe3e279c98263b8fc2f3547f6a68103526f2
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-11 20:34:43 +0200

    Patch pgo01org training initialization
    Cf. <https://github.com/intel/php_pgo_training_scripts/pull/5>.

--- pgo/cases/pgo01org/TrainingCaseHandler.php
+++ pgo/cases/pgo01org/TrainingCaseHandler.php
@@ -103,6 +103,12 @@ class TrainingCaseHandler extends Abstracts\TrainingCase implements Interfaces\T
 		$standard_calls = preg_replace(",parse_str\(\\\$var1\),", "parse_str(\$var1, \$dummy)", $standard_calls);
 		file_put_contents($fl, $standard_calls);
 
+		// patch <https://github.com/intel/php_pgo_training_scripts/pull/5>
+		$fl = $htdocs . DIRECTORY_SEPARATOR . "init.php";
+		$standard_calls = file_get_contents($fl);
+		$standard_calls = preg_replace(",^initDB\(\);$,m", "mysqli_report(MYSQLI_REPORT_OFF);\n$0", $standard_calls);
+		file_put_contents($fl, $standard_calls);
+
 		//$php->exec($cmd, NULL, $env);
 		/* TODO check status or switch to cli. */
 		$out = file_get_contents("http://$http_host:$http_port/init.php");
