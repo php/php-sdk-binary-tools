bc287aa8859d299e3a2ba10270e72c0614a063d6
Shivam Mathur <shivam_jpr@hotmail.com>
2026-07-17 15:27:15 +0530

    Add SBOM generation tool (#49)

---
+++ bin/phpsdk_sbom.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cmd /c "exit /b 0"
+
+if "%PHP_SDK_PHP_CMD%"=="" (
+	call %~dp0phpsdk_setvars.bat
+	if "!PHP_SDK_PHP_CMD!"=="" (
+		echo PHP SDK is not setup
+		exit /b 3
+	)
+)
+
+call %PHP_SDK_PHP_CMD% %PHP_SDK_BIN_PATH%\phpsdk_sbom.php %*
+
+exit /b %errorlevel%
